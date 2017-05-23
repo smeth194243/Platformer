fa3cf82ae42ca64c7d7fde0f79424e3ab5b0182d
smeth194243 <smor7432@740s-pl-106.local>
2017-05-23 13:54:23 -0600

    Big update.

--- Platformer/Model/Player.cs
+++ Platformer/Model/Player.cs
@@ -70,7 +70,7 @@ namespace Platformer
         private const float AirDragFactor = 0.58f;
 
         // Constants for controlling vertical movement
-        private const float MaxJumpTime = 0.35f;
+        private const float MaxJumpTime = 0.7f;
         private const float JumpLaunchVelocity = -3500.0f;
         private const float GravityAcceleration = 3400.0f;
         private const float MaxFallSpeed = 550.0f;
